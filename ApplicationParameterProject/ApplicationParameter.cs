5b4efecaf07e7dd24dbfdd36b168a73d3a803312
Sanalog <bilgi@sanalog.org>
2016-07-30 21:11:50 +0300

    New Update
    Free Use

---
+++ ApplicationParameterProject/ApplicationParameter.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Sanalog
+{
+    [ToolboxData("<{0}:ApplicationParameter runat=server></{0}:ApplicationParameter>")]
+    public class ApplicationParameter : Parameter
+    {
+        public string AppKeyName { get; set; }
+
+        public ApplicationParameter()
+        {
+            if (HttpContext.Current.Application[AppKeyName] != null)
+                this.DefaultValue = HttpContext.Current.Application[AppKeyName].ToString();
+            else
+                this.DefaultValue = "31";
+
+            this.Type = System.TypeCode.Int16;
+        }
+    }
+}
